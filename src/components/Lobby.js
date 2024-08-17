054c5268ef17547bd4569f7fcc4baccdc1eac1d8
Orshimon810 <127754114+Orshimon810@users.noreply.github.com>
2024-08-17 16:26:09 +0300

    changing urls to endpoints

--- src/components/Lobby.js
+++ src/components/Lobby.js
@@ -9,7 +9,7 @@ const Lobby = () => {
   useEffect(() => {
     const fetchCodeBlocks = async () => {
       try {
-        const response = await fetch('http://localhost:3001/api/codeblocks');
+        const response = await fetch('https://jslecturer-639a06a0d162.herokuapp.com/api/codeblocks');
         const data = await response.json();
         setCodeBlocks(data);
       } catch (error) {
